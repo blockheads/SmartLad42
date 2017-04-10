f223513834766e40f0f6fc8d6b2d842efbaec279
businessfish <noahmichaels2.0@gmail.com>
2017-04-09 20:40:46 -0400

    more spelling errors

--- words.js
+++ words.js
@@ -46,10 +46,10 @@ var scotsman = ["Mr.","pajama-wearin'","basket-face","slipper-wieldin'",
 
 var scotsmanNouns = ["apple-john","baggage","barnacle","bladder","boar-pig","bugbear","bum-bailey",
                      "canker-blossom","clack-dish","clotpole","coxcomb","codpiece","death-token",
-                     "dewberry","flap-dragon","flax-wench","flirt-gill","floot-licker","fustilarian",
+                     "dewberry","flap-dragon","flax-wench","flirt-gill","foot-licker","fustilarian",
                      "giglet","gudgeon","haggard","harpy","hedge-pig","horn-beast","hugger-mugger",
                      "hoithead","lewdster","lout","maggot-pie","malt-worm","mammet","measle","minnow",
-                     "miscreant","moldwarp","mumble-news","nut-hook","pigeaon-egg","pignut","puttock",
+                     "miscreant","moldwarp","mumble-news","nut-hook","pigeon-egg","pignut","puttock",
                      "pumpion","ratsbane","scut","skainsmate","strumpet","varlot","vassal","whey-face","wagtail"];
 
 
